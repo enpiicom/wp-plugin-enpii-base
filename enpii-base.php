12f444110bd16fe8266cac347370757cd8770773
Trac Nguyen <npbtrac@yahoo.com>
2022-12-09 13:39:42 +0700

    Add Wp cli

--- enpii-base.php
+++ enpii-base.php
@@ -9,44 +9,28 @@
  * Text Domain: enpii
  */
 
-use Enpii\Wp_Plugin\Enpii_Base\Libs\Wp_Application;
-
 defined( 'DIR_SEP' ) || define( 'DIR_SEP', DIRECTORY_SEPARATOR );
-defined( 'WP_APP_FORCE_CREATE_WP_APP_FOLDER' ) || define( 'WP_APP_FORCE_CREATE_WP_APP_FOLDER', true );
 
 defined( 'ENPII_BASE_SLUG' ) || define( 'ENPII_BASE_SLUG', 'enpii-base' );
-
-// We want to have helper functions before loading the dependancies (for overridding purposes)
-require_once __DIR__ . DIR_SEP . 'src' . DIR_SEP . 'Support' . DIR_SEP . 'helpers-wp-app.php';
-require_once __DIR__ . DIR_SEP . 'src' . DIR_SEP . 'Support' . DIR_SEP . 'helpers.php';
+defined( 'ENPII_BASE_VERSION' ) || define( 'ENPII_BASE_SLUG', '0.0.1' );
 
 // We include composer autoload here
 require_once __DIR__ . DIR_SEP . 'vendor' . DIR_SEP . 'autoload.php';
 
-$wp_app_base_path = enpii_base_get_wp_app_base_path();
-if ( WP_APP_FORCE_CREATE_WP_APP_FOLDER ) {
-	enpii_base_prepare_wp_app_folders( $wp_app_base_path );
-}
+/**
+ | WP CLI handlers
+ |
+ */
+add_action( 'cli_init', 'enpii_base_wp_cli_register_commands' );
 
-$config = [
+/**
+ | Create a wp_app() instance to be used in the whole application
+ */
+$wp_app_base_path = enpii_base_wp_app_get_base_path();
+$config = apply_filters( 'enpii_base_wp_app_prepare_config', [
 	'app' => require_once __DIR__ . DIR_SEP . 'wp-app-config' . DIR_SEP . 'app.php',
-	// 'cache' => require_once __DIR__ . DIR_SEP . 'wp-app-config'. DIR_SEP .'cache.php',
-	// 'logging'          => require_once __DIR__ . DIR_SEP . 'wp-app-config' . DIR_SEP . 'logging.php',
-	// 'session' => require_once __DIR__ . DIR_SEP . 'wp-app-config'. DIR_SEP .'session.php',
-	// 'view'             => require_once __DIR__ . DIR_SEP . 'wp-app-config' . DIR_SEP . 'view.php',
-];
-
-$config = apply_filters( 'enpii_base_wp_app_prepare_config', $config );
+] );
 $wp_app = ( new \Enpii\Wp_Plugin\Enpii_Base\Libs\Wp_Application( $wp_app_base_path ) )->init_config( $config );
 
 // We register Enpii_Base plugin as a Service Provider
 $wp_app->register( \Enpii\Wp_Plugin\Enpii_Base\Base\Plugin::class );
-
-// We want to have the wp_app working before other plugins start their operations
-// add_action(
-// 	'after_setup_theme',
-// 	function () use ( $config ) {
-// 		enpii_base_setup_wp_app( $config );
-// 	},
-// 	1000
-// );
--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -2,28 +2,16 @@
 
 declare(strict_types=1);
 
-use Enpii\Wp_Plugin\Enpii_Base\App\Http\Controllers\Index_Controller;
 use Enpii\Wp_Plugin\Enpii_Base\Base\Plugin;
+use Enpii\Wp_Plugin\Enpii_Base\Base\Wp_Cli\Enpii_Base_Wp_Cli;
 use Enpii\Wp_Plugin\Enpii_Base\Dependencies\Illuminate\Filesystem\Filesystem;
-use Enpii\Wp_Plugin\Enpii_Base\Dependencies\Illuminate\Support\Facades\Route;
 use Enpii\Wp_Plugin\Enpii_Base\Libs\Wp_Application;
 
-if ( ! function_exists( 'enpii_base_get_wp_app_base_path' ) ) {
-	function enpii_base_get_wp_app_base_path() {
-		return rtrim( WP_CONTENT_DIR, '/' ) . DIR_SEP . 'uploads' . DIR_SEP . 'wp-app';
-	}
-}
-
-if ( ! function_exists( 'enpii_base_get_wp_app_storage_path' ) ) {
-	function enpii_base_get_wp_app_storage_path() {
-		return enpii_base_get_wp_app_base_path() . DIR_SEP . 'storage';
-	}
-}
-
-if ( ! function_exists( 'enpii_base_prepare_wp_app_folders' ) ) {
-	function enpii_base_prepare_wp_app_folders( string $wp_app_base_path ): void {
+if ( ! function_exists( 'enpii_base_wp_app_prepare_folders' ) ) {
+	function enpii_base_wp_app_prepare_folders( string $wp_app_base_path ): void {
 		$file_system = new Filesystem();
 		$file_system->ensureDirectoryExists( $wp_app_base_path, 0755 );
+
 		$file_system->ensureDirectoryExists( $wp_app_base_path . DIR_SEP . 'bootstrap', 0755 );
 		$file_system->ensureDirectoryExists( $wp_app_base_path . DIR_SEP . 'bootstrap' . DIR_SEP . 'cache', 0777 );
 
@@ -39,6 +27,12 @@ if ( ! function_exists( 'enpii_base_prepare_wp_app_folders' ) ) {
 		$file_system->chmod( $wp_app_base_path . DIR_SEP . 'storage', 0777 );
 		$file_system->chmod( $wp_app_base_path . DIR_SEP . 'storage' . DIR_SEP . 'framework', 0777 );
 		$file_system->chmod( $wp_app_base_path . DIR_SEP . 'storage' . DIR_SEP . 'logs', 0777 );
+	}
+}
+
+if ( ! function_exists( 'enpii_base_wp_app_get_base_path' ) ) {
+	function enpii_base_wp_app_get_base_path() {
+		return rtrim( WP_CONTENT_DIR, '/' ) . DIR_SEP . 'uploads' . DIR_SEP . 'wp-app';
 	}
 }
 
@@ -59,9 +53,8 @@ if ( ! function_exists( 'enpii_base_setup_wp_app' ) ) {
 	}
 }
 
-if ( ! function_exists( 'enpii_base_register_wp_app_routes' ) ) {
-	function enpii_base_register_wp_app_routes(): void {
-		Route::get('/', [Index_Controller::class, 'home']);
-		Route::get('/home', [Index_Controller::class, 'home']);
+if ( ! function_exists( 'enpii_base_wp_cli_register_commands' ) ) {
+	function enpii_base_wp_cli_register_commands(): void {
+		\WP_CLI::add_command( 'enpii-base', Enpii_Base_Wp_Cli::class );
 	}
 }
