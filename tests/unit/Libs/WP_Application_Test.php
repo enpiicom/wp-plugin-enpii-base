7750548dad5de25e725c72e093c283392cfa7440
Loi Le, Thi Kim <loi.le@enpii.com>
2023-03-09 15:53:35 +0700

    Add test for handle registering plugin as service provider method

--- tests/unit/Libs/WP_Application_Test.php
+++ tests/unit/Libs/WP_Application_Test.php
@@ -3,8 +3,10 @@
 namespace Enpii\WP_Plugin\Enpii_Base\Tests\Unit\Base\Libs;
 
 use Codeception\Stub;
+use Enpii\WP_Plugin\Enpii_Base\Base\Plugin;
 use Enpii\Wp_Plugin\Enpii_Base\Dependencies\Illuminate\Cache\CacheServiceProvider;
 use Enpii\Wp_Plugin\Enpii_Base\Dependencies\Illuminate\Foundation\Application;
+use Enpii\WP_Plugin\Enpii_Base\Dependencies\Illuminate\Support\ServiceProvider;
 use Enpii\Wp_Plugin\Enpii_Base\Libs\WP_Application;
 
 class WP_Application_Test extends \Codeception\Test\Unit {
@@ -31,6 +33,23 @@ class WP_Application_Test extends \Codeception\Test\Unit {
 
 		// We need to ensure all the Cache service provider is registered
 		$this->assertNotEmpty($this->wp_app->getProviders(CacheServiceProvider::class), 'Cache Service is not registered');
+	}
+
+	public function test_register_plugin_service_provider(): void {
+		$config = [
+			'env' => 'local',
+		];
+		$wp_app = $this->wp_app->init_config($config);
+		$plugin = new Plugin( $wp_app );
+		$plugin->set_base_path( __DIR__ );
+		$plugin->set_base_url( $this->wp_app_base_path );
+		$wp_app->register( $plugin );
+
+		// We need to ensure all the Cache service provider is registered
+		$this->assertEquals($config['env'], $this->wp_app['config']['env'], 'Config is not correct');
+		$this->assertEquals(__DIR__ , $plugin->get_base_bath() , 'Base path is not correct');
+		$this->assertEquals($this->wp_app_base_path , $plugin->get_base_url() , 'Base url is not correct');
+		$this->assertNotEmpty($this->wp_app->getProviders(Plugin::class), 'Plugin is not registered');
 	}
 
 	public function test_resource_path() {
