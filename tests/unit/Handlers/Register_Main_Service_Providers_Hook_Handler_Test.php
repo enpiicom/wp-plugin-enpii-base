dd9c4446552237f2bc919943e3ea2b3f6af8895d
Loi Le, Thi Kim <loi.le@enpii.com>
2023-03-13 14:42:02 +0700

    Update var name

--- tests/unit/Handlers/Register_Main_Service_Providers_Hook_Handler_Test.php
+++ tests/unit/Handlers/Register_Main_Service_Providers_Hook_Handler_Test.php
@@ -12,8 +12,8 @@ use Enpii\WP_Plugin\Enpii_Base\Tests\Support\Unit\Libs\Unit_Test_Case;
 
 class Register_Main_Service_Providers_Hook_Handler_Test extends Unit_Test_Case {
 	public function test_handle(): void {
-		$genenicCommand = new Generic_Command($this->wp_app);
-		( new Register_Main_Service_Providers_Handler() )->handle($genenicCommand);
+		$genenic_command = new Generic_Command($this->wp_app);
+		( new Register_Main_Service_Providers_Handler() )->handle($genenic_command);
 
 		// We need to ensure all the main service providers are registered
 		$this->assertNotEmpty( $this->wp_app->getProvider( Log_Service_Provider::class ), 'Log Service is not registered' );
