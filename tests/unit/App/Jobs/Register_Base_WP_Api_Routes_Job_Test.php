4198500397c00bf59f07c2c4faf0ddae178e66c4
Loi Le, Thi Kim <loi.le@enpii.com>
2023-12-07 16:21:10 +0700

    Add Register_Base_WP_Api_Routes_Job_Test file

---
+++ tests/unit/App/Jobs/Register_Base_WP_Api_Routes_Job_Test.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Enpii_Base\Tests\Unit\App\Jobs;
+
+use Enpii_Base\App\Jobs\Process_WP_App_Request_Job;
+use Enpii_Base\Foundation\Shared\Base_Job;
+use Enpii_Base\Tests\Support\Unit\Libs\Unit_Test_Case;
+
+class Register_Base_WP_Api_Routes_Job_Test extends Unit_Test_Case {
+
+	public function test_handle() {
+	}
+}
