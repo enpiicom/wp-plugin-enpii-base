dde4b6454097c72ead14678ecbd170341c9c1784
Trac Nguyen <npbtrac@yahoo.com>
2023-12-25 22:09:19 +0700

    Fix phpcs

--- src/App/Exceptions/Simple_Exception.php
+++ src/App/Exceptions/Simple_Exception.php
@@ -26,28 +26,24 @@ class Simple_Exception extends Exception {
 	 * @return void
 	 */
 	public function __construct(
-        $error_message = null,
+		$error_message = null,
 		$http_code = 400,
-        $error_code = 400,
-        array $errors = [],
-        Exception $previous = null,
-        array $extra_data = []
-    ) {
-        $this->http_code = $http_code;
-		$this->error_message =$error_message;
-        $this->error_code = !empty($error_code) ? $error_code : $http_code;
-        $this->errors = $errors;
-        $this->previous = $previous;
-        $this->extra_data = $extra_data;
+		$error_code = 400,
+		array $errors = [],
+		Exception $previous = null,
+		array $extra_data = []
+	) {
+		$this->http_code = $http_code;
+		$this->error_message = $error_message;
+		$this->error_code = ! empty( $error_code ) ? $error_code : $http_code;
+		$this->errors = $errors;
+		$this->previous = $previous;
+		$this->extra_data = $extra_data;
 
-		// parent::__construct( (string) $error_message, (int) $error_code, $previous);
-    }
+		parent::__construct( (string) $error_message, (int) $error_code, $previous );
+	}
 
 	public function __toString() {
-		return sprintf('Exception thrown, code: [%s], message: %s at class %s', $this->error_code, $this->error_message, __CLASS__) ."\n";
-    }
-
-	protected function is_console(): bool {
-		return \PHP_SAPI === 'cli' || \PHP_SAPI === 'phpdbg';
+		return sprintf( 'Exception thrown, code: [%s], message: %s at class %s', $this->error_code, $this->error_message, __CLASS__ ) . "\n";
 	}
 }
