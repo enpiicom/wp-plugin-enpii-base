f53e4a9317ab8241b6839011b82be187f072ec13
Trac Nguyen <npbtrac@yahoo.com>
2024-02-04 23:10:59 +0700

    Update something

--- src/Foundation/Support/helpers-utils.php
+++ src/Foundation/Support/helpers-utils.php
@@ -6,6 +6,7 @@
 
 declare(strict_types=1);
 
+use Enpii_Base\App\Support\Enpii_Base_Helper;
 use Symfony\Component\VarDumper\Caster\ReflectionCaster;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\CliDumper;
@@ -41,16 +42,25 @@ if ( ! function_exists( 'devdd' ) ) {
 	}
 }
 
-if ( ! function_exists( 'dev_error_log' ) ) {
-	function dev_error_log( ...$vars ): void {
-		// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_debug_backtrace
-		$dev_trace = debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT, 1 );
+if ( ! function_exists( 'dev_var_dump') ) {
+	function dev_var_dump($var, int $max_depth = 3): string {
 		$dumper = new CliDumper();
 		$cloner = new VarCloner();
 		$cloner->addCasters( ReflectionCaster::UNSET_CLOSURE_FILE_INFO );
 
+		return $dumper->dump( $cloner->cloneVar( $var )->withMaxDepth( $max_depth ), true );
+	}
+}
+
+if ( ! function_exists( 'dev_error_log' ) ) {
+	function dev_error_log( ...$vars ): void {
+		// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_debug_backtrace
+		$dev_trace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 0 );
+
 		$log_message = '';
-		$log_message .= "Debugging dev_error_log \n======= Dev logging start here \n" . $dev_trace[0]['file'] . ':' . $dev_trace[0]['line'] . " \n";
+		$log_message .= "Debugging dev_error_log, url (". Enpii_Base_Helper::get_current_url() .") \n======= Dev logging start here \n" . $dev_trace[0]['file'] . ':' . $dev_trace[0]['line'] . " \n";
+		unset( $dev_trace[0]);
+		$log_message .= "Trace :" . dev_var_dump( $dev_trace ) . " \n";
 		foreach ( $vars as $index => $var ) {
 			$dump_content = null;
 			if ( $var === false ) {
@@ -59,7 +69,7 @@ if ( ! function_exists( 'dev_error_log' ) ) {
 				$type = is_object( $var ) ? get_class( $var ) : gettype( $var );
 
 				// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_export
-				$dump_content = $dumper->dump( $cloner->cloneVar( $var ), true );
+				$dump_content = dev_var_dump( $var );
 			}
 			$log_message .= "Var no $index: type " . $type . ' - ' . $dump_content . " \n";
 		}
