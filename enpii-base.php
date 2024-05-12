6a6408dd36900381166ee81cd50ce8a8e353a1ec
Trac Nguyen <npbtrac@yahoo.com>
2024-05-12 17:36:59 +0700

    Fix the wp-app prefix for multisite

--- enpii-base.php
+++ enpii-base.php
@@ -5,7 +5,7 @@
  * Description: Base plugin for WP development using Laravel
  * Author:      dev@enpii.com, nptrac@yahoo.com
  * Author URI:  https://enpii.com/enpii-team/
- * Version:     0.6.1
+ * Version:     0.6.2
  * Text Domain: enpii
  */
 
