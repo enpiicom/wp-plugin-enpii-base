a3e4a6761ab279634952463c0ed94211c6cf76c8
Trac Nguyen <npbtrac@yahoo.com>
2024-05-21 18:18:07 +0700

    Fix phpcs

--- tests/Unit/Sample_Test.php
+++ tests/Unit/Sample_Test.php
@@ -7,9 +7,9 @@ namespace Enpii_Base\Tests\Unit;
 use Enpii_Base\Tests\Support\Unit\Libs\Unit_Test_Case;
 use Mockery;
 
-class Sample_Test extends Unit_Test_Case
-{
-    protected function setUp(): void {
+class Sample_Test extends Unit_Test_Case {
+
+	protected function setUp(): void {
 	}
 
 	protected function tearDown(): void {
@@ -18,6 +18,6 @@ class Sample_Test extends Unit_Test_Case
 
 	public function test_something() {
 		// Assetions go here
-		$this->assertTrue(true);
+		$this->assertTrue( true );
 	}
 }
