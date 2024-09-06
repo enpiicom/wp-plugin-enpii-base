f8fcdcd96239a02a5f1f1211aec417d1d2a8895d
Loi Le, Thi Kim <loi.le@enpii.com>
2024-09-06 10:26:06 +0700

    Add Init_WP_App_Kernels_Action test

--- src/App/Actions/Init_WP_App_Kernels_Action.php
+++ src/App/Actions/Init_WP_App_Kernels_Action.php
@@ -2,12 +2,13 @@
 
 namespace Enpii_Base\App\Actions;
 
+use Enpii_Base\Foundation\Actions\Base_Action;
 use Enpii_Base\Foundation\Support\Executable_Trait;
 
 /**
  * @method static function exec(): void
  */
-class Init_WP_App_Kernels_Action {
+class Init_WP_App_Kernels_Action extends Base_Action {
 	use Executable_Trait;
 
 	/**
