199eed7b7ed4ae6ae04f05f00bf12c9ede82d70b
Loi Le, Thi Kim <loi.le@enpii.com>
2024-09-09 15:56:26 +0700

    Add Setup_WP_App_In_Console_Action_Test

--- tests/Unit/App/Actions/Setup_WP_App_In_Console_Action_Test.php
+++ tests/Unit/App/Actions/Setup_WP_App_In_Console_Action_Test.php
@@ -4,20 +4,30 @@ declare(strict_types=1);
 
 namespace Enpii_Base\Tests\Unit\App\Actions;
 
+use Enpii_Base\App\Actions\Mark_Setup_WP_App_Done_Action;
 use Enpii_Base\App\Actions\Setup_WP_App_In_Console_Action;
+use Enpii_Base\App\WP\WP_Application;
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 use Enpii_Base\Tests\Support\Unit\Libs\Unit_Test_Case;
 use InvalidArgumentException;
+
 use Mockery;
+use WP_Mock;
 
 class Setup_WP_App_In_Console_Action_Test extends Unit_Test_Case {
 	protected $console_command;
+	protected $filesystem;
+
 
 	protected function setUp(): void {
 		parent::setUp();
 
 		// Mock the Console Command
 		$this->console_command = Mockery::mock( Command::class );
+
+		// Mock the Filesystem
+		$this->filesystem = Mockery::mock( Filesystem::class );
 	}
 
 	protected function tearDown(): void {
@@ -44,7 +54,82 @@ class Setup_WP_App_In_Console_Action_Test extends Unit_Test_Case {
 	 * @preserveGlobalState disabled
 	 */
 	public function test_handle() {
-		
+	}
+
+	public function test_perform_setup_actions(): void {
+		// Expect comments and calls for publishing Laravel and Enpii Base migrations and assets
+		$this->console_command->shouldReceive( 'comment' )
+			->with( 'Publishing Laravel Migrations...' )
+			->once();
+		$this->console_command->shouldReceive( 'call' )
+			->with(
+				'vendor:publish',
+				[
+					'--tag' => 'laravel-migrations',
+					'--force' => true,
+				] 
+			)
+			->once();
+
+		$this->console_command->shouldReceive( 'comment' )
+			->with( 'Publishing Laravel Assets...' )
+			->once();
+		$this->console_command->shouldReceive( 'call' )
+			->with(
+				'vendor:publish',
+				[
+					'--tag' => 'laravel-assets',
+					'--force' => true,
+				] 
+			)
+			->once();
+
+		$this->console_command->shouldReceive( 'comment' )
+			->with( 'Publishing Enpii Base Migrations...' )
+			->once();
+		$this->console_command->shouldReceive( 'call' )
+			->with(
+				'vendor:publish',
+				[
+					'--tag' => 'enpii-base-migrations',
+					'--force' => true,
+				] 
+			)
+			->once();
+
+		$this->console_command->shouldReceive( 'comment' )
+			->with( 'Publishing Enpii Base Assets...' )
+			->once();
+		$this->console_command->shouldReceive( 'call' )
+			->with(
+				'vendor:publish',
+				[
+					'--tag' => 'enpii-base-assets',
+					'--force' => true,
+				] 
+			)
+			->once();
+
+		$this->console_command->shouldReceive( 'comment' )
+			->with( 'Doing Migrations...' )
+			->once();
+		$this->console_command->shouldReceive( 'call' )
+			->with(
+				'migrate',
+				[
+					'--no-interaction' => true,
+					'--force' => true,
+					'--step' => true,
+				] 
+			)
+			->once();
+
+		// Create the action class
+		$action = Mockery::mock( Setup_WP_App_In_Console_Action::class )->makePartial();
+
+		// Call perform_setup_actions with the mocked console command
+		$action->perform_setup_actions( $this->console_command );
+
 		$this->assertTrue( true );
 	}
 }
