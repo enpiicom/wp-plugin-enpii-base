2b0ede1cbb734f0f8687833b8c5ff5b59b931b49
Loi Le, Thi Kim <loi.le@enpii.com>
2025-02-14 12:24:33 +0700

    Refactor code

--- resources/views/errors/layout-minimal-error.blade.php
+++ resources/views/errors/layout-minimal-error.blade.php
@@ -19,7 +19,7 @@
 		<!-- Google Font Import -->
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Raleway">
 
 	</head>
 	<body style="background-color: #fff; color: #636b6f; font-family: 'Raleway', sans-serif; font-weight: 100; height: 100vh; margin: 0;">
